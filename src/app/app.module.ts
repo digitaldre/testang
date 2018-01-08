e52028bc6859999cfb8e2f8d716ed5e93c1aeab4
Andre Reihing <reihing@hotmail.com>
2018-01-08 12:53:19 -0500

    Components Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material'; 
+import { MatToolbarModule, MatListModule, MatMenuModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatMenuModule,
+    MatListModule,
     FlexLayoutModule
   ],
   providers: [],
