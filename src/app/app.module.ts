207640e2d61d792f90158a3a1a3df47212635165
Andre Reihing <reihing@hotmail.com>
2018-01-08 14:58:42 -0500

    Components Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule, MatListModule, MatMenuModule } from '@angular/material'; 
+import { MatToolbarModule, MatListModule, MatGridListModule, MatMenuModule, MatButtonModule, MatCardModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
@@ -21,6 +21,9 @@ import { MenuComponent } from './menu/menu.component';
     MatToolbarModule,
     MatMenuModule,
     MatListModule,
+    MatGridListModule,
+    MatButtonModule, 
+    MatCardModule,
     FlexLayoutModule
   ],
   providers: [],
