19533c9d76388a7b70e9b7bca48fe70b13062178
Andre Reihing <reihing@hotmail.com>
2018-01-10 03:20:08 -0500

    Angular Router Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatListModule, MatGridListModule, MatMenuModule, MatButtonModule, MatCardModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { DishService } from './services/dish.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import 'hammerjs';
 
@@ -12,6 +13,10 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 
 @NgModule({
@@ -20,7 +25,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -32,9 +40,9 @@ import { FooterComponent } from './footer/footer.component';
     MatButtonModule, 
     MatCardModule,
     FlexLayoutModule,
-    
+    AppRoutingModule    
   ],
-  providers: [DishService],
-  bootstrap: [AppComponent]
+  providers: [ DishService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
